e35d32d4af4ddb6337764a4367a9dd918f565ff1
salvatore rivera <salvatorecch@gmail.com>
2026-05-13 11:46:52 -0600

    calculadora

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 
 export function addTwoNumbers(/*aqui tambien va código*/){
+  let n1 = parseInt(prompt("Ingresa el primer valor"));
+  let n2 = parseInt(prompt("Ingresa el segundo valor"));
+
+  let suma = n1 + n2;
+  console.log(suma);
+  
   //aqui va tu código
 }
+
+addTwoNumbers();
